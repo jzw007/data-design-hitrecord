95578d8ff8a9734b6cd740137671b632e9a43127
jeremiahwood <jzwood47@cnm.edu>
2016-10-12 13:08:47 -0600

    First set up

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<!--add CSS file-->
+
+	</head>
+	<body>
+		<!-- This is the Header-->
+		<header>
+			<h1></h1>
+		</header>
+		<!-- this is the main page content-->
+		<main>
+
+			<section>
+				<!--begin section 2-->
+			</section>
+			<h2></h2>
+
+			<section>
+				<!--begin section 3-->
+			</section>
+				<section></section>
+
+		</main>
+	</body>
+</html>
