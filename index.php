fa010a0e2085e8f90f700c1f6b8b30021fcc41c4
jeremiahwood <jzwood47@cnm.edu>
2016-10-12 22:31:11 -0600

    Persona done

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 	<head>
 		<meta charset="UTF-8"/>
 		<!--add CSS file-->
+		<title>Persona
+			<p>Name: Ryan Kilgore</p>
+			<p>Age: 22</p>
+			<p>Proffesion: Works one job as a barista, the second job as a waiter and takes classes at the local community college.</p>
+			<p>Technology: He has a nikon 5700 that he uses to record and edits his footage on his mac book pro.</p>
+			<p> Frustraiotion: He has tried to create a following on youtube and raise money on gofundme.com for his projects and dreams with not much luck on either end.</p>
+		<p>Goal: To produce an idea from the screen play then preproduction then watch it come to life through filming and finally distribution of the film.  </p>
+			<p> </p>
+		</title>
 
 	</head>
-	<body>
+		<body>
 		<!-- This is the Header-->
 		<header>
 			<h1></h1>
@@ -24,5 +33,5 @@
 				<section></section>
 
 		</main>
-	</body>
+		</body>
 </html>
