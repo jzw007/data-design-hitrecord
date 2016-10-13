da93834fbc5c0d378aa13d52fcb2e63aedf28e8b
jeremiahwood <jzwood47@cnm.edu>
2016-10-13 12:00:01 -0600

    Persona updated

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 	<head>
 		<meta charset="UTF-8"/>
 		<!--add CSS file-->
-		<title>Persona
-			<p>Name: Ryan Kilgore</p>
-			<p>Age: 22</p>
-			<p>Proffesion: Works one job as a barista, the second job as a waiter and takes classes at the local community college.</p>
-			<p>Technology: He has a nikon 5700 that he uses to record and edits his footage on his mac book pro.</p>
-			<p> Frustraiotion: He has tried to create a following on youtube and raise money on gofundme.com for his projects and dreams with not much luck on either end.</p>
-		<p>Goal: To produce an idea from the screen play then preproduction then watch it come to life through filming and finally distribution of the film.  </p>
-			<p> </p>
-		</title>
+		<title>Data Design of Hitrecord</title>
+		</head>
 
-	</head>
 		<body>
+			<header>Persona </header>
+			<li>Name: Ryan Kilgore</li>
+			<li>Age: 22</li>
+			<li>Proffesion: Works one job as a barista, the second job as a waiter and takes classes at the local community college.</li>
+			<li>Technology: He has a nikon 5700 that he uses to record and edits his footage on his mac book pro.</li>
+			<li> Frustration: He has tried to use youtube as a way to promote himself and gofundme to raise money for his projects and dreams with not much luck with either method.</li>
+		<li>Goal: The ultimate goal for him is to be a working actor, weather from getting a roll through auditioning or writing his own  scene and or script that he gets to star in. With Hitrecord its more of a collaboration of artists working together for the bigger picture.</li>
+		</body>
 		<!-- This is the Header-->
 		<header>
 			<h1></h1>
